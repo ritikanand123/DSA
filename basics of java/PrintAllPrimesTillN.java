cb7b69404d53c9824ab46df5be93fe82d64089e2
Ritik Anand <104204145+ritikanand123@users.noreply.github.com>
2022-04-23 11:06:33 +0530

    Create PrintAllPrimesTillN.java

---
+++ basics of java/PrintAllPrimesTillN.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scan = new Scanner(System.in);
+        int start =scan.nextInt();
+        int end =scan.nextInt();
+        for(int n=start;n<=end;n++){
+            int count =0;
+            
+            for(int div=2;div*div<=n;div++){
+                if(n%div==0){
+                    count++;
+                    break;
+                }
+            }
+            
+            
+            
+            
+            if(count==0){
+                System.out.println(n);
+            }
+        }
+    }
+}

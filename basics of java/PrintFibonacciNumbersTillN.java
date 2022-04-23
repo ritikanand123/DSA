923f1b733c39b93dac2dd4d9ffdf1b2d61e6b84a
Ritik Anand <104204145+ritikanand123@users.noreply.github.com>
2022-04-23 11:08:14 +0530

    Create PrintFibonacciNumbersTillN.java

---
+++ basics of java/PrintFibonacciNumbersTillN.java
@@ -0,0 +1,25 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      // write your code here
+      Scanner scan = new Scanner(System.in);
+      int num=scan.nextInt();
+      System.out.println("0");
+      
+      int first=1;
+      System.out.println(1);
+      System.out.println(1);
+      int second=1;
+      int count=3;
+      while(count<num){
+        
+        int sum=first+second;
+        System.out.println(sum);
+        first=second;
+        second=sum;
+        count++;
+      }
+   }
+  }

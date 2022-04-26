bf0335474c3b88c186116b8b2fd2b56c6db4944c
Ritik Anand <104204145+ritikanand123@users.noreply.github.com>
2022-04-26 06:29:50 +0530

    Create StateOfWakanda.java

---
+++ 2-D Arrays/StateOfWakanda.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scan = new Scanner(System.in);
+        int row=scan.nextInt();
+        int col = row;
+        int arr[][]=new int[row][row];
+        for(int i = 0 ;i<arr.length;i++){
+            for(int j= 0;j<arr[0].length;j++){
+                arr[i][j]=scan.nextInt();
+            }
+        }
+        
+        
+        for(int k = 0;k<arr.length;k++){
+         for(int i = 0 , j = k ;i<arr.length-k;i++,j++){
+            System.out.println(arr[i][j]);
+         }
+        }
+    }
+
+}

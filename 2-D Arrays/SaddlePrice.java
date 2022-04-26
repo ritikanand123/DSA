42514a3358fe2c4203b873175861e129c07d7815
Ritik Anand <104204145+ritikanand123@users.noreply.github.com>
2022-04-26 19:51:23 +0530

    Create SaddlePrice.java

---
+++ 2-D Arrays/SaddlePrice.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int rowcol = scan.nextInt();
+        int arr[][]= new int[rowcol][rowcol];
+        for(int i =0 ;i<arr.length;i++){
+            for(int j = 0 ; j <arr[0].length;j++){
+                arr[i][j]=scan.nextInt();
+            }
+        }
+        
+        for(int i  = 0 ;i<arr.length;i++){
+            int smallestincol=0;
+             for(int j = 1 ; j<arr[0].length;j++){
+                if(arr[i][j]<arr[i][smallestincol]){
+                    smallestincol=j;
+                }
+            }
+        boolean result=true;
+        for(int k=0;k<arr.length;k++){
+            if(arr[i][smallestincol]<arr[k][smallestincol]){
+                result =false;
+                break;
+            }
+            
+        }
+            if(result==true){
+                System.out.println(arr[i][smallestincol]);
+                return;
+            }
+        }
+        System.out.println("Invalid input");
+}
+}

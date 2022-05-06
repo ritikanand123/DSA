b19894f9d2ae1976fbc65c222d5f3457f27e5858
Ritik Anand <104204145+ritikanand123@users.noreply.github.com>
2022-05-06 06:08:43 +0530

    Update climbStairs.java

--- DynamicProgramming-Greedy/climbStairs.java
+++ DynamicProgramming-Greedy/climbStairs.java
@@ -7,25 +7,49 @@ public class Main {
         
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
-        int paths=stairpathsTab(num);
+        int paths=stairpaths(num,new int[num+1]);
         System.out.println(paths);
     }
+    
+    public static int stairpaths(int n , int [] arr){
+        
+        if(n==0){
+            return 1;
+        }if(n<0){
+            return 0;
+        }
+        if(arr[n]>0){
+            return arr[n];
+        }
+        
+        int p1 = stairpaths(n-1,arr);
+        int p2 = stairpaths(n-2,arr);
+        int p3 = stairpaths(n-3,arr);
+        int cp = p1+p2+p3; 
+        arr[n]=cp;
+        
+        return cp;
+        
+        
+    } 
+    
     public static int stairpathsTab(int num){
         int dp []= new int[num+1];
         dp[0]=1;
         
-        for(int i = 1 ; i <= num ;i++){
+        for(int i = 0 ; i <= num ;i++){
             if(i==1){
                 dp[i]=dp[i-1];
             }
-            else if(i==2){
+            if(i==2){
                 dp[i]=dp[i-1]+dp[i-2];
             }
-            else{
+            if(i>=3){
                 dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
             }
         }
-        return dp[num];
+        
+        return dp[num+1];
         
         
         

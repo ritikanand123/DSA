ce75672994f0b5a303f4c741c660ad7e6451caf3
Ritik Anand <104204145+ritikanand123@users.noreply.github.com>
2022-05-18 21:04:35 +0530

    Create BuyAndSellStocks -InfiniteTransactionsAllowed.java

---
+++ DynamicProgramming-Greedy/BuyAndSellStocks -InfiniteTransactionsAllowed.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int arr[]=new int [size];
+        
+        for(int i = 0 ;i < arr.length ;i++){
+            arr[i]=scan.nextInt();
+        }
+        
+        int bd=0;
+        int sd=0;
+        int profit=0;
+        
+        for(int i = 1 ;  i < arr.length ;i++){
+            
+            if(arr[i]>=arr[i-1]){
+                sd++;
+            }else{
+                profit+=arr[sd]-arr[bd];
+                sd=bd=i;
+            }
+            
+            
+        }
+        profit+=arr[sd]-arr[bd];
+        System.out.println(profit);
+        
+    }
+
+}

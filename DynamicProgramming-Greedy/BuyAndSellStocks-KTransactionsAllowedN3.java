b4ca86728b6b32725c5fbd1599e8055ad540bb11
Ritik Anand <104204145+ritikanand123@users.noreply.github.com>
2022-05-23 05:46:33 +0530

    Create BuyAndSellStocks-KTransactionsAllowedN3.java

---
+++ DynamicProgramming-Greedy/BuyAndSellStocks-KTransactionsAllowedN3.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int n =scan.nextInt();
+        int arr[]=new int[n];
+        for(int i =0 ;i< arr.length ;i++){
+            arr[i]=scan.nextInt();
+        }
+        int trns=scan.nextInt();
+        
+        int dp[][]=new int[trns+1][n];
+        
+        for(int i =1 ;i <= trns ;i++){
+            for(int j=1;j< arr.length ;j++){
+               int max = dp[i][j-1];
+               
+               for(int d=0;d<j;d++){
+                   
+                   int pps= dp[i-1][d];//previous profit stored
+                   int pttrns=arr[j]-arr[d];//profit this transaction
+                   if((pps+pttrns)>max){
+                       max=pps+pttrns;
+                   }
+               }
+               
+                dp[i][j]=max;
+               
+            }
+            
+        }
+        
+        System.out.println(dp[trns][n-1]);
+    }
+
+}
+
+
+
